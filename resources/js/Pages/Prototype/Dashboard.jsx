f84f4a7bf6a71f7fb2a0d2290f687eae26f28c25
Andriawan Firmansyah <andriawnfirmansyah@gmail.com>
2022-09-05 19:52:44 +0700

    feat: slicing dashboard-browse movie

--- resources/js/Pages/Prototype/Dashboard.jsx
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -2,6 +2,7 @@ import Authenticated from "@/Layouts/Authenticated/Index";
 import Flickity from "react-flickity-component";
 import { Head } from "@inertiajs/inertia-react";
 import FeaturedMovie from "@/Components/FeaturedMovie";
+import MovieCard from "@/Components/MovieCard";
 
 export default function Dashboard() {
     const flickityOption = {
@@ -16,20 +17,43 @@ export default function Dashboard() {
     return (
         <Authenticated>
             <Head>
-            <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css" />
+                <link
+                    rel="stylesheet"
+                    href="https://unpkg.com/flickity@2/dist/flickity.min.css"
+                />
             </Head>
+            <title>Dashboard</title>
             <div>
                 <div className="font-semibold text-[22px] text-black mb-4">
                     Featured Movies
                 </div>
                 <Flickity className="gap-[30px]" options={flickityOption}>
                     {[1, 2, 3, 4].map((i) => (
-                        <FeaturedMovie key={i} 
-                        slug="The-batman-in-love" 
-                        name={`The Batman In Love ${i}`} 
-                        category="Action" 
-                        thumbnail="https://images.unsplash.com/photo-1509347528160-9a9e33742cdb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" 
-                        rating={i + 1} />
+                        <FeaturedMovie
+                            key={i}
+                            slug="The-batman-in-love"
+                            name={`The Batman In Love ${i}`}
+                            category="Action"
+                            thumbnail="https://images.unsplash.com/photo-1509347528160-9a9e33742cdb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                            rating={i + 1}
+                        />
+                    ))}
+                </Flickity>
+            </div>
+            <div className="mt-[50px]">
+                <div className="font-semibold text-[22px] text-black mb-4">
+                    Browse
+                </div>
+                <Flickity className="gap-[30px]" options={flickityOption}>
+                    {/* Movie 1 */}
+                    {[1, 2, 3, 4, 5, 6].map((i) => (
+                        <MovieCard
+                            key={i}
+                            slug="The-batman-in-love"
+                            name={`The Batman In Love ${i}`}
+                            category="Action"
+                            thumbnail="https://images.unsplash.com/photo-1509347528160-9a9e33742cdb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                        />
                     ))}
                 </Flickity>
             </div>
