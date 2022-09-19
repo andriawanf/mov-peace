507a818b2bff1362bbb2e9702556f8fd6e9628c1
Andriawan Firmansyah <andriawnfirmansyah@gmail.com>
2022-09-19 20:37:21 +0700

    update

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -52,6 +52,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect('/dashboard');
+        return redirect(RouteServiceProvider::HOME);
     }
 }
